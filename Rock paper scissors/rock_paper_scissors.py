0f452f991d4cdb3e7e2cf77e64c9deacf376f479
alvyynm <alvyynm@gmail.com>
2022-06-25 08:09:49 +0300

    Initial commit

---
+++ Rock paper scissors/rock_paper_scissors.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+    ''' Plays one round of the game '''
+    user_input = input("'r' for rock, 's' for scissors, 'p' for paper")
+    computer_choice = random.choice(['r','s','p'])
+    
+    def game(user, opponent):
+        ''' Determines who wins the game (r>s s>p p>r) '''
+        if (user == 'r' and opponent == 's'):
+            print("You win!")
+        elif (user == 's' and opponent == 'p'):
+            print("You win")
+        elif (user == 'p' and opponent == 'r'):
+            print("You win")
+        elif (user == opponent):
+            print("It's a draw")
+        else:
+            print("You lost!! OOps!")
+            
+    return game(user_input, computer_choice)
+    
+play()
